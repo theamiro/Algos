e520f0282b236fba7e755accbb72f4766373c09e
Michael Amiro <odindo.michael@gmail.com>
2022-03-11 09:50:30 +0300

    Added more algo playgrounds

--- BinarySearch.playground/Contents.swift
+++ BinarySearch.playground/Contents.swift
@@ -25,3 +25,33 @@ func binarySearch(value: Int, in array: [Int]) -> Bool {
 }
 
 print(binarySearch(value: 4, in: array))
+
+
+let anArray = [2,3,5,6,7,8,9,10, 0]
+
+func binSearch(_ value: Int, in array: [Int]) -> Bool{
+    if array.isEmpty {
+        return false
+    }
+    array.sorted(by: <)
+    
+    var leftIndex = 0
+    var rightIndex = array.count - 1
+    
+    while leftIndex < rightIndex {
+        let middleIndex = (leftIndex + rightIndex)/2
+        let middleValue = array[middleIndex]
+        
+        if middleValue == value {
+            return true
+        }
+        if value < middleValue {
+            rightIndex = middleIndex
+        }
+        if value > middleValue {
+            leftIndex = middleIndex
+        }
+    }
+    return false
+}
+binSearch(8, in: anArray)
--- Fizzbuzz.playground/Contents.swift
+++ Fizzbuzz.playground/Contents.swift
@@ -2,7 +2,6 @@ import UIKit
 import Foundation
 
 func fizzbuzz(max: Int) {
-    
     for i in 1...max {
         if (i % 15 == 0) {
             print("FizzBuzz")
@@ -16,4 +15,25 @@ func fizzbuzz(max: Int) {
     }
 }
 
-fizzbuzz(max: 20)
+//fizzbuzz(max: 20)
+
+func fizzbuzzer(max: Int) {
+    if max < 1 {
+        print(0)
+        return
+    }
+    for i in 1...max {
+        if i % 15 == 0 {
+            print("Fizzbuzz")
+        } else if i % 3 == 0 {
+            print("Fizz")
+        } else if i % 5 == 0{
+            print("Buzz")
+        } else {
+            print("\(i)")
+        }
+    }
+}
+fizzbuzzer(max: 0)
+print("----------------")
+fizzbuzzer(max: 50)
